f6c15693afa111ce043f41f4c3669c0d2c191713
NiKOLOZJanga <nikoloz.djangavadze@gmail.com>
2024-11-22 21:49:21 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./componenrs/Header";
+import Main from "./componenrs/Main"
+import Navigation from "./componenrs/Navigation";
+
+const App = () => {
+  return (
+    <div className="App">
+    <Header  />
+    <Main  />
+  </div>
+  );
+};
+
+export default App;
